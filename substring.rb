49f2a6993b45bd9dbd3234227543bcb747a68202
Tuna Fortuna <tuna.fortuna@groundfloor.us>
2023-01-19 09:48:14 -0500

    fix: update substring

--- substring.rb
+++ substring.rb
@@ -1,29 +1,21 @@
-array = ["hel", "lo", "the", "e"]
+dictionary = ["hel", "lo", "the", "e"]
 string = "HeLlo There old Friend!"
-def createMap(array)
-  hashMap = Hash.new
-  for word in array
-    hashMap[word] = 0
-  end
-  hashMap
-end
 
-def subStringChecker(hashmap, stringArray)
-  keys = hashmap.keys
-  for key in keys
+def subStringChecker(dictionary, stringArray)
+  hashMap = Hash.new
+  for key in dictionary
     for string in stringArray
       if string.upcase.match(/#{key.upcase}/)
-        hashmap[key] += string.upcase.scan(/#{key.upcase}/).size
+        hashMap[key] = string.upcase.scan(/#{key.upcase}/).size
       end  
     end
   end  
-  puts hashmap
+  puts hashMap
 end 
 
 def substring(string, dictionary)
-  hashMap = createMap(dictionary)
   strings = string.split
-  subStringChecker(hashMap, strings)
+  subStringChecker(dictionary, strings)
 end
 
-substring(string, array)
+substring(string, dictionary)
